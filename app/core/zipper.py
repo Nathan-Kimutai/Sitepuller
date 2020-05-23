674f35e0462fe6ca0858a696bc78f3591053a80c
Nathan Kimutai <nathanpykimutai@gmail.com>
2020-05-23 14:05:08 +0300

    writing some todos

--- app/core/zipper.py
+++ app/core/zipper.py
@@ -16,4 +16,13 @@ class MyZipper:
         Write files passed to the method as arguments to the
         zip file object
         """
-        this.zip_obj.write(filename)
+        ## TODO: I cant be opening and closing a zip_obj each and every time
+        ## # TODO: Make sure that I write all the files and then close it after all the files
+        ## have been written to the object
+        try:
+            this.zip_obj.write(filename)
+        except Exception as err:
+            ## TODO: Add a real logging here and not print the error functionality
+            print(err)
+        finally:
+            this.zip_obj.close()
