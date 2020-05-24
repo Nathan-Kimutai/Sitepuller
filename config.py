37a17cd0db58933ff55afaf20c1924a2b3afb91e
Nathan Kimutai <nathanpykimutai@gmail.com>
2020-05-24 13:25:56 +0300

    worked on the development configruation class

--- config.py
+++ config.py
@@ -1,13 +1,27 @@
+import os
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
+    """
+    This is the base configuration class
+    """
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     @staticmethod
     def init_app(app):
         pass
 
 class DevConfig(Config):
+    """
+    Development Configuration class
+    """
     DEBUG=True
+    SQLALCHEMY_DATABASE_URI = None # Later I will add a database uri perferably MySQL
 
 class TestConfig(Config):
-    pass
+    TESTING = True
+    DEBUG = True
 
 config = {
     "default":DevConfig,
